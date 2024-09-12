85473bf8acce841a6bf52ab6a47637fa4af362c3
Mattéo Blivet <blivet.pro@gmail.com>
2024-09-12 16:18:12 +0200

    Update gitignore

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,135 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID C1S1 = 4159720949U;
+        static const AkUniqueID C1S2 = 4159720950U;
+        static const AkUniqueID C1S3 = 4159720951U;
+        static const AkUniqueID C1S4 = 4159720944U;
+        static const AkUniqueID C1S5 = 4159720945U;
+        static const AkUniqueID C1S7 = 4159720947U;
+        static const AkUniqueID C1S8 = 4159720956U;
+        static const AkUniqueID C1S9 = 4159720957U;
+        static const AkUniqueID C1S10 = 1145747615U;
+        static const AkUniqueID C1S11 = 1145747614U;
+        static const AkUniqueID C1S12 = 1145747613U;
+        static const AkUniqueID C1S13 = 1145747612U;
+        static const AkUniqueID C1S14 = 1145747611U;
+        static const AkUniqueID C1S15 = 1145747610U;
+        static const AkUniqueID C1S16 = 1145747609U;
+        static const AkUniqueID C1S17 = 1145747608U;
+        static const AkUniqueID C1S61 = 1095414727U;
+        static const AkUniqueID C1S62 = 1095414724U;
+        static const AkUniqueID C2S1 = 3757205154U;
+        static const AkUniqueID C2S2 = 3757205153U;
+        static const AkUniqueID C2S3 = 3757205152U;
+        static const AkUniqueID C2S4 = 3757205159U;
+        static const AkUniqueID C2S5 = 3757205158U;
+        static const AkUniqueID C2S7 = 3757205156U;
+        static const AkUniqueID C2S8 = 3757205163U;
+        static const AkUniqueID C2S9 = 3757205162U;
+        static const AkUniqueID C2S10 = 748130614U;
+        static const AkUniqueID C2S11 = 748130615U;
+        static const AkUniqueID C2S12 = 748130612U;
+        static const AkUniqueID C2S13 = 748130613U;
+        static const AkUniqueID C2S14 = 748130610U;
+        static const AkUniqueID C2S15 = 748130611U;
+        static const AkUniqueID C2S16 = 748130608U;
+        static const AkUniqueID C2S17 = 748130609U;
+        static const AkUniqueID C2S61 = 798463374U;
+        static const AkUniqueID C2S62 = 798463373U;
+        static const AkUniqueID C4S1 = 3756322616U;
+        static const AkUniqueID C4S2 = 3756322619U;
+        static const AkUniqueID C4S3 = 3756322618U;
+        static const AkUniqueID C4S4 = 3756322621U;
+        static const AkUniqueID C4S5 = 3756322620U;
+        static const AkUniqueID C4S7 = 3756322622U;
+        static const AkUniqueID C4S8 = 3756322609U;
+        static const AkUniqueID C4S9 = 3756322608U;
+        static const AkUniqueID C4S10 = 2909050136U;
+        static const AkUniqueID C4S11 = 2909050137U;
+        static const AkUniqueID C4S12 = 2909050138U;
+        static const AkUniqueID C4S13 = 2909050139U;
+        static const AkUniqueID C4S14 = 2909050140U;
+        static const AkUniqueID C4S15 = 2909050141U;
+        static const AkUniqueID C4S16 = 2909050142U;
+        static const AkUniqueID C4S17 = 2909050143U;
+        static const AkUniqueID C4S61 = 3026493468U;
+        static const AkUniqueID C4S62 = 3026493471U;
+        static const AkUniqueID C5S1 = 3756469649U;
+        static const AkUniqueID C5S2 = 3756469650U;
+        static const AkUniqueID C5S3 = 3756469651U;
+        static const AkUniqueID C5S4 = 3756469652U;
+        static const AkUniqueID C5S5 = 3756469653U;
+        static const AkUniqueID C5S7 = 3756469655U;
+        static const AkUniqueID C5S8 = 3756469656U;
+        static const AkUniqueID C5S9 = 3756469657U;
+        static const AkUniqueID C5S10 = 166509427U;
+        static const AkUniqueID C5S11 = 166509426U;
+        static const AkUniqueID C5S12 = 166509425U;
+        static const AkUniqueID C5S13 = 166509424U;
+        static const AkUniqueID C5S14 = 166509431U;
+        static const AkUniqueID C5S15 = 166509430U;
+        static const AkUniqueID C5S16 = 166509429U;
+        static const AkUniqueID C5S17 = 166509428U;
+        static const AkUniqueID C5S61 = 250397459U;
+        static const AkUniqueID C5S62 = 250397456U;
+        static const AkUniqueID C6S1 = 3890837758U;
+        static const AkUniqueID C6S2 = 3890837757U;
+        static const AkUniqueID C6S3 = 3890837756U;
+        static const AkUniqueID C6S4 = 3890837755U;
+        static const AkUniqueID C6S5 = 3890837754U;
+        static const AkUniqueID C6S7 = 3890837752U;
+        static const AkUniqueID C6S8 = 3890837751U;
+        static const AkUniqueID C6S9 = 3890837750U;
+        static const AkUniqueID C6S10 = 310999018U;
+        static const AkUniqueID C6S11 = 310999019U;
+        static const AkUniqueID C6S12 = 310999016U;
+        static const AkUniqueID C6S13 = 310999017U;
+        static const AkUniqueID C6S14 = 310999022U;
+        static const AkUniqueID C6S15 = 310999023U;
+        static const AkUniqueID C6S16 = 310999020U;
+        static const AkUniqueID C6S17 = 310999021U;
+        static const AkUniqueID C6S61 = 227110858U;
+        static const AkUniqueID C6S62 = 227110857U;
+        static const AkUniqueID C7S1 = 4025205711U;
+        static const AkUniqueID C7S2 = 4025205708U;
+        static const AkUniqueID C7S3 = 4025205709U;
+        static const AkUniqueID C7S4 = 4025205706U;
+        static const AkUniqueID C7S5 = 4025205707U;
+        static const AkUniqueID C7S7 = 4025205705U;
+        static const AkUniqueID C7S8 = 4025205702U;
+        static const AkUniqueID C7S9 = 4025205703U;
+        static const AkUniqueID C7S10 = 2133147373U;
+        static const AkUniqueID C7S11 = 2133147372U;
+        static const AkUniqueID C7S12 = 2133147375U;
+        static const AkUniqueID C7S13 = 2133147374U;
+        static const AkUniqueID C7S14 = 2133147369U;
+        static const AkUniqueID C7S15 = 2133147368U;
+        static const AkUniqueID C7S16 = 2133147371U;
+        static const AkUniqueID C7S17 = 2133147370U;
+        static const AkUniqueID C7S61 = 2015704041U;
+        static const AkUniqueID C7S62 = 2015704042U;
+        static const AkUniqueID PLAY_TC_SFX_RC_CONTROL_CAMERANEXT = 3941019376U;
+        static const AkUniqueID PLAY_TC_SFX_RC_CONTROL_HELPBUTTON = 902629463U;
+        static const AkUniqueID PLAY_TC_SFX_RC_CONTROL_SLIDERAM = 3348876875U;
+        static const AkUniqueID PLAY_TC_SFX_RC_CONTROL_SLIDERPM = 3634096260U;
+        static const AkUniqueID PLAY_TC_SFX_RC_CONTROL_TAPERUNOUT = 1909750219U;
+        static const AkUniqueID PLAY_TC_SFX_RC_CONTROL_TICKCLOCKWISE = 919921797U;
+        static const AkUniqueID PLAY_TC_SFX_RC_CONTROL_TICKCLOCKWISEMINUTES = 1808890972U;
+        static const AkUniqueID PLAY_TC_SFX_RC_CONTROL_TICKCOUNTERCLOCKWISE = 3773613759U;
+        static const AkUniqueID PLAY_TC_SFX_RC_CONTROL_TICKCOUNTERCLOCKWISEMINUTES = 2677265010U;
         static const AkUniqueID PLAY_TESTEVENT = 2351864238U;
     } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID CLOCKSPEED = 2485212962U;
+        static const AkUniqueID RTPC_AMB_VOL = 4239806503U;
+        static const AkUniqueID RTPC_DIALOGUE_VOL = 3553633719U;
+        static const AkUniqueID RTPC_MASTER_VOL = 460070935U;
+        static const AkUniqueID RTPC_MUSIC_VOL = 3061620274U;
+        static const AkUniqueID RTPC_SFX_VOL = 3502123226U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
@@ -24,7 +151,11 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID AMB = 1117531639U;
+        static const AkUniqueID DIALOGUE = 3930136735U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
